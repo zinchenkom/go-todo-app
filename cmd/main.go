2253e6c2c4ac1105b8e9a6618fe50452a16a6bfb
maxim <myemail@gmail.com>
2022-05-18 20:14:06 +0300

    Routes and db structs added

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"github.com/zinchenkom/go-todo-app"
+	"github.com/zinchenkom/go-todo-app/pkg/handler"
 	"log"
 )
 
-
-func main(){
+func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatal("error occured while running http server %s", err.Error())
 	}
 }
